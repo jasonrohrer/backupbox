1fb64a79459126c190a14488ca93f11f9095f000
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-03-24 22:21:00 -0700

    Fixed rsync to not make sub-folder.  Added file count to printed report.

--- suCronJob.sh
+++ suCronJob.sh
@@ -8,9 +8,11 @@ date=`date +"%Y_%b_%d_%a"`
 
 mkdir $date
 
-rsync -av --delete /home/backupuser/backups/$date $date
+rsync -av --delete /home/backupuser/backups/$date/* $date/
 
 backupSize=`du -sb $date | sed -e 's/\t.*//'| numfmt --to=iec-i | sed -e 's/\([0-9.]*\)/\1 /'`
+
+backupCount=`ls -1 $date | wc -l`
 
 remainSize=`df / | grep "/" | awk '{print $4}'`
 
@@ -24,6 +26,7 @@ reportDate=`date | fmt -w 24`
 echo -e "\
 $reportDate\n\
 Backup folder:\n     $date\nsafely archived.\n\
+${backupCount} files.\n\
 ${backupSize}B in this backup.\n\
 ${remainSize}B left on disk.\n\n" | lpr -P epson
 
